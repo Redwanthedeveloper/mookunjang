c0aca3f8ed8fa2987444de128a83235057d0e13c
MD. Redwanul Haque <71332783+Redwanthedeveloper@users.noreply.github.com>
2022-01-06 15:07:29 +0600

    countdown updated error fixed

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -1,38 +1,38 @@
 import React from 'react';
-import Countdown from 'react-countdown';
+// import Countdown from 'react-countdown';
 
 const CountDown = () => {
-  const renderer = ({ days, hours, minutes }) => {
-    return (
-      <div className='CountDown__timer px-4 md:px-0 flex gap-16 items-center justify-center py-12 text-white'>
-        <div className='text-center'>
-          <p className='CountDown__timer-main text-3xl lg:text-7xl font-medium mb-1'>
-            {days}
-          </p>
-          <p className='uppercase text-xs md:text-base font-light gray-100'>
-            Days
-          </p>
-        </div>
-        <div className='text-center'>
-          <p className='CountDown__timer-main text-3xl lg:text-7xl font-medium mb-1'>
-            {hours}
-          </p>
-          <p className='uppercase text-xs md:text-base font-light gray-100'>
-            hours
-          </p>
-        </div>
+  // const renderer = ({ days, hours, minutes }) => {
+  //   return (
+  //     <div className='CountDown__timer px-4 md:px-0 flex gap-16 items-center justify-center py-12 text-white'>
+  //       <div className='text-center'>
+  //         <p className='CountDown__timer-main text-3xl lg:text-7xl font-medium mb-1'>
+  //           {days}
+  //         </p>
+  //         <p className='uppercase text-xs md:text-base font-light gray-100'>
+  //           Days
+  //         </p>
+  //       </div>
+  //       <div className='text-center'>
+  //         <p className='CountDown__timer-main text-3xl lg:text-7xl font-medium mb-1'>
+  //           {hours}
+  //         </p>
+  //         <p className='uppercase text-xs md:text-base font-light gray-100'>
+  //           hours
+  //         </p>
+  //       </div>
 
-        <div className='text-center'>
-          <p className='CountDown__timer-main text-3xl lg:text-7xl font-medium mb-1'>
-            {minutes}
-          </p>
-          <p className='uppercase text-xs md:text-base font-light gray-100'>
-            minutes
-          </p>
-        </div>
-      </div>
-    );
-  };
+  //       <div className='text-center'>
+  //         <p className='CountDown__timer-main text-3xl lg:text-7xl font-medium mb-1'>
+  //           {minutes}
+  //         </p>
+  //         <p className='uppercase text-xs md:text-base font-light gray-100'>
+  //           minutes
+  //         </p>
+  //       </div>
+  //     </div>
+  //   );
+  // };
   return (
     <>
       <div className='CountDown py-12' style={{}}>
